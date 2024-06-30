dae3e427a9f449597458be961e5dc59156a8dc21
10urkarasu <10urkarasu@gmail.com>
2024-06-30 17:01:08 +0300

    created api logic

---
+++ server.js
@@ -0,0 +1,16 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const app = require('./app');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose.connect();
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
